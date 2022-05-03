87c0a9e56fc80c5d7584fa5c69cfd092ea5fdc14
TR537 <73218369+TR537@users.noreply.github.com>
2022-05-03 10:13:49 +0200

    Turned PD game into a single player game against a bot

--- prisoner/__init__.py
+++ prisoner/__init__.py
@@ -1,5 +1,5 @@
 from otree.api import *
-
+import random
 
 doc = """
 This is a repeated PD game
@@ -16,7 +16,7 @@ class Subsession(BaseSubsession):
     pass
 
 class Group(BaseGroup):
-    treatment = models.BooleanField()
+    pass
 
 
 class Player(BasePlayer):
@@ -25,18 +25,11 @@ class Player(BasePlayer):
         doc="""This player's decision""",
         widget=widgets.RadioSelect,
     )
+    other_coop = models.BooleanField()
+    other_payoff = models.CurrencyField()
 
 
 # FUNCTIONS
-def set_payoffs(group: Group):
-    for p in group.get_players():
-        set_payoff(p)
-
-
-def other_player(player: Player):
-    return player.get_others_in_group()[0]
-
-
 def set_payoff(player: Player):
     session = player.session
     payoff_matrix = {
@@ -45,52 +38,32 @@ def set_payoff(player: Player):
         (False, False): session.payoff_matrix['both_defect'],
         (True, False): session.payoff_matrix['I_coop'],
     }
-    other = other_player(player)
-    player.payoff = payoff_matrix[(player.cooperate, other.cooperate)]
+    if random.random() < 0.3:
+        player.other_coop = True
+
+    player.payoff = payoff_matrix[(player.cooperate, player.other_coop)]
+    player.other_payoff = payoff_matrix[(player.other_coop, player.cooperate)]
 
 
 # PAGES
-class GroupingWaitPage(WaitPage):
-    group_by_arrival_time = True
-    body_text = '''You might wait for a while on this waitpage until someone else joins the game.
-                   Please don't get discouraged and don't leave the site. You should still not exceed the 30 minutes and you will be paid at least USD 3.00.'''
-
-    @staticmethod
-    def after_all_players_arrive(group: Group):
-        # save each participant's current group ID so it can be
-        # accessed in the next app.
-        for p in group.get_players():
-            participant = p.participant
-            participant.past_group_id = group.id
-            if (participant.past_group_id % 2) ==0:
-                participant.treatment = True
-            else:
-                participant.treatment = False
-
-    @staticmethod
-    def is_displayed(player):
-        return player.round_number == 1
-
 class Decision(Page):
     form_model = 'player'
     form_fields = ['cooperate']
 
 
 class ResultsWaitPage(WaitPage):
-    after_all_players_arrive = set_payoffs
+    after_all_players_arrive = set_payoff
 
 
 class Results(Page):
     @staticmethod
     def vars_for_template(player: Player):
-        opponent = other_player(player)
         return dict(
-            opponent=opponent,
-            same_choice=player.cooperate == opponent.cooperate,
-            my_decision=player.field_display('cooperate'),
-            opponent_decision=opponent.field_display('cooperate'),
+            same_choice=player.cooperate == player.other_coop,
+            my_decision=player.cooperate,
+            opponent_decision=player.other_coop,
         )
 
 
 
-page_sequence = [GroupingWaitPage, Decision, ResultsWaitPage, Results]
+page_sequence = [Decision, ResultsWaitPage, Results]
